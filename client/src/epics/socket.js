c2f21bb65a57a5802b0643f1c1f16aee7a0cd045
Mariano Heller <marianofheller@gmail.com>
2018-06-13 10:33:28 -0300

    rxjs, it worksgit status!

--- client/src/epics/socket.js
+++ client/src/epics/socket.js
@@ -1,72 +1,36 @@
 import io from 'socket.io-client';
 import { Observable } from 'rxjs/Observable';
+import { race } from 'rxjs/observable/race';
+import { concat } from 'rxjs/observable/concat';
+
 
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/observable/of';
-
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/switchMap';
+
 
 import * as socketActions from '../actions/socket';
 
 
-
-
-
-
 export default action$ => action$
-    .ofType(socketActions.INIT_SOCKET.REQUEST)
-    .map(() => io.connect('http://localhost:3001'))
-    .switchMap(socket => 
+  .ofType(socketActions.INIT_SOCKET.REQUEST)
+  .map(() => io.connect('http://localhost:3001'))
+  .switchMap(socket => (
+    race(
       Observable.fromEvent(socket, 'connect_error')
         .do(() => socket.disconnect())
-        .map(() => socketActions.init.failure({ connection: 'connect_failed' }))
-    );
-    
-    /*
-          const ioClient = io.connect('http://localhost:3001');
-          ioClient.on('connection', (socket) => {
-          console.log('connection!');
-          observer.next(socketActions.init.success(socket));
-          socket.off('connect');
-          observer.complete();
-        }); */
-
-/* 
-
-    .switchMap(() =>
-      Observable.create((observer) => {
-        io.connect('http://localhost:3001');
-        io.on('connection', (socket) => {
-          Observable.fromEvent(socket, 'connect')
-            .do(() => console.log('CONNECTed!!!'))
-            .filter(data => data === true)
-            .forEach(data => data);
-
-          Observable.fromEvent(socket, 'connect_failed')
-            .do(() => console.log('Connection FAILED!!!'))
-            .filter(data => data === true)
-            .forEach(data => data);
-        });
-
-        
-        socket.on('connect', () => {
-          
-          observer.next(socketActions.init.success(socket));
-          socket.off('connect');
-          observer.complete();
-        });
-        socket.on('connect_failed', () => {
-          console.log("CONNTECT FAILED");
-          observer.next(socketActions.init.failure({ connection: 'connect_failed' }));
-          socket.off('connect_failed');
-          observer.error();
-        });
-      })); 
-*/
-
-
+        .map(() => socketActions.init.failure({ connection: 'connect_failed' })),
+      Observable.fromEvent(socket, 'connect')
+        .map(() => socketActions.init.success({ socket })),
+    )
+  ))
+  .ofType(socketActions.INIT_SOCKET.FAILURE)
+  .mergeMap(failureAction =>
+    concat(
+      Observable.of(failureAction),
+      Observable.of(socketActions.init.request()).delay(10000),
+    ));
